ca5176fe9d7117ba019e0d24db34708effed546e
Brian Hicks <brian@brianthicks.com>
2017-10-23 09:44:47 -0500

    cask + pallet

---
+++ Cask
@@ -0,0 +1,5 @@
+(source gnu)
+(source melpa)
+
+(depends-on "pallet")
+(depends-on "use-package")
---
+++ init.el
@@ -0,0 +1,16 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; first we're gonna get cask and pallet set up correctly following the instructions at http://www.lambdacat.com/modern-emacs-package-management-with-cask-and-pallet/
+;;
+;;I've installed cask with homebrew, so it's in a weird location under /usr/local.
+(require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
+(cask-initialize)
+
+(require 'pallet)
+(pallet-mode t)
+
+;; use-package!
