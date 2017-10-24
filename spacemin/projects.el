5ec361b1e23d533204113f2286ae77d7f7233f73
Brian Hicks <brian@brianthicks.com>
2017-10-24 14:19:21 -0500

    shorten projectile modeline

--- spacemin/projects.el
+++ spacemin/projects.el
@@ -4,7 +4,7 @@
 
   ;; workaround for https://github.com/bbatsov/projectile/issues/1183
   (setq projectile-mode-line
-	'(:eval (format "Projectile[%s]" (projectile-project-name))))
+	'(:eval (projectile-project-name)))
 
   (evil-leader/set-key
    "p!" 'projectile-run-shell-command-in-root
