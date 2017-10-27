ebe868c3c08749ceb752ad72ee7f6cdfbb55360f
Brian Hicks <brian@brianthicks.com>
2017-10-26 19:33:07 -0500

    PokeVars! gotta set 'em all!

--- spacemin/ruby.el
+++ spacemin/ruby.el
@@ -14,7 +14,9 @@
 	;; https://github.com/zenspider/enhanced-ruby-mode/issues/36
 	enh-ruby-comment-column 32
 	enh-ruby-bounce-deep-indent t
-	enh-deep-indent-parent t
+	enh-ruby-deep-indent-paren t
+	enh-ruby-deep-indent-construct t
+	enh-ruby-hanging-paren-deep-indent-level 1
 	enh-ruby-hanging-brace-deep-indent-level 1
 	enh-ruby-hanging-brace-indent-level 2
 	enh-ruby-hanging-indent-level 2
