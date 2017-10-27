db838c6fbe99b83f1a8520c42e7bbfb0b6d2cbc1
Brian Hicks <brian@brianthicks.com>
2017-10-26 19:17:44 -0500

    make Ruby indentation not weird
    Welcome to Brian Hicks' School For Emacs Who Don't Indent Good And
    Want To Do Other Stuff Good Too™

--- spacemin/ruby.el
+++ spacemin/ruby.el
@@ -2,7 +2,17 @@
   :mode ("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode)
   :interpreter ("ruby" . enh-ruby-mode)
   :config
-  (setq enh-ruby-add-encoding-comment-on-save nil)
+  (setq enh-ruby-add-encoding-comment-on-save nil
+
+	;; https://github.com/zenspider/enhanced-ruby-mode/issues/36
+	enh-ruby-comment-column 32
+	enh-ruby-bounce-deep-indent t
+	enh-deep-indent-parent t
+	enh-ruby-hanging-brace-deep-indent-level 1
+	enh-ruby-hanging-brace-indent-level 2
+	enh-ruby-hanging-indent-level 2
+	enh-ruby-hanging-paren-indent-level 2
+	enh-ruby-indent-level 2)
 
   (use-package rbenv
     :config
