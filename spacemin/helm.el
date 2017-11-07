f949f81e934037c185a9d59c42c035a461c0327b
Brian Hicks <brian@brianthicks.com>
2017-11-06 21:42:24 -0600

    match some more fuzzy stuff in helm

--- spacemin/helm.el
+++ spacemin/helm.el
@@ -10,7 +10,9 @@
   :config
   (helm-mode 1)
 
-  (setq helm-buffers-fuzzy-matching t
+  (setq helm-M-x-fuzzy-match t
+	helm-buffers-fuzzy-matching t
+	helm-recentf-fuzzy-match t
 	helm-file-cache-fuzzy-match t
 	helm-window-prefer-horizontal-split t)
 
@@ -19,7 +21,10 @@
     "<SPC>" 'helm-M-x
 
     ;; *b*uffers
-    "bb" 'helm-buffers-list
+    "bb" 'helm-mini
+
+    ;; *y*anking
+    "yp" 'helm-show-kill-ring
     )
 
   (use-package helm-projectile
