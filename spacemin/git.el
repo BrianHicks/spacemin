900fe0fff498cab60a43e7a4c039c1d0fd97ac72
Brian Hicks <brian@brianthicks.com>
2017-11-06 22:59:03 -0600

    move git stuff to where straight can see it

--- spacemin/git.el
+++ spacemin/git.el
@@ -1,11 +1,20 @@
-;; magit is great! Hooray!
-(use-package magit
+;;; magit --- the best frontend for git in emacs
+
+;;; Commentary:
+;;; omg it's so good I love it
+
+;;; Code:
+
+(use-package magit)
+
+(use-package evil-magit
   :config
-  (evil-leader/set-key "gs" 'magit-status)
-  (use-package evil-magit))
+  (evil-leader/set-key "gs" 'magit-status))
 
 ;; TODO:
 ;;
 ;; - github integration
 ;; - shortcut to jump to a PR for a branch
 ;; - shortcut to jump to a Jenkins build for a branch
+
+;;; git.el ends here
