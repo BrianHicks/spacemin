53ec71c227920dce50e33158343a43b37c58291a
Brian Hicks <brian@brianthicks.com>
2017-11-06 23:01:18 -0600

    move markdown stuff to where straight can see it

--- spacemin/markdown.el
+++ spacemin/markdown.el
@@ -1,7 +1,16 @@
+;;; markdown --- kinda nice, really.
+
+;;; Commentary:
+;;; not a lot to see here, move along.
+
+;;; Code:
+
 (use-package markdown-mode
-  :mode ("\\.md\\'" . markdown-mode)
-  :config
-  (use-package markdown-toc
+  :mode ("\\.md\\'" . markdown-mode))
+
+(use-package markdown-toc
     :config
     (evil-leader/set-key-for-mode 'markdown-mode
-      "met" 'markdown-toc-generate-or-refresh-toc)))
+      "met" 'markdown-toc-generate-or-refresh-toc))
+
+;;; markdown.el ends here
