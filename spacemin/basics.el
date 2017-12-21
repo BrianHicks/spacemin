3717822c96a1298395f99c3ccb8664211f2b14ad
Brian Hicks <brian@brianthicks.com>
2017-12-21 15:51:14 -0600

    ¯\_(ツ)_/¯

--- spacemin/basics.el
+++ spacemin/basics.el
@@ -11,8 +11,6 @@
 ;; PATH twice. It's useful for me in particular because git is set to
 ;; sign each commit with GPG, which is installed in a non-default
 ;; (homebrewed) path.
-;;
-;; we do this here instead of in a sub-file because
 (use-package exec-path-from-shell
   :config
   (when (memq window-system '(mac ns x))
