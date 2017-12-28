accc9b87af596f2d33178c5850772094988c1874
Brian Hicks <brian@brianthicks.com>
2017-12-28 14:36:27 -0600

    port elm

---
+++ lang/elm.el
@@ -0,0 +1,32 @@
+;;; elm --- types and tangrams
+
+;;; Commentary:
+;;; it's Elm!
+
+;;; Code:
+
+(use-package elm-mode
+  :bind-prefix-local
+  ("c" '(:ignore t :which-key "compile")
+   "cc" 'elm-compile-buffer
+
+   "e" '(:ignore t :which-key "edit")
+   "ea" 'elm-compile-add-annotations
+   "eI" 'elm-compile-clean-imports
+   "ef" 'elm-mode-format-buffer)
+
+  :config
+  (setq elm-tags-on-save t
+	elm-format-on-save t)
+
+  (add-to-list 'company-backends 'company-elm))
+
+(use-package flycheck-elm
+  :config
+  ;; TODO: this isn't working. Why?
+  (with-eval-after-load 'flycheck
+    '(add-hook 'flycheck-mode-hook #'flycheck-elm-setup)))
+
+(provide 'elm)
+
+;;; elm.el ends here
