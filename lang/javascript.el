f524dc1c4df6977e0267a2af35789e0801f7f418
Brian Hicks <brian@brianthicks.com>
2018-01-04 22:17:12 -0600

    remove tern. I can't figure out how to install it correctly and it looks dead.

--- lang/javascript.el
+++ lang/javascript.el
@@ -15,23 +15,6 @@
   :config
   (add-hook 'js2-mode-hook 'prettier-js-mode))
 
-(use-package tern
-  :after js2-mode
-  :general
-  (general-nvmap :keympas 'tern-mode-map
-		 "gd" 'tern-find-definition)
-
-  (general-nvmap :keymaps 'tern-mode-map
-		 :prefix ","
-		 "h" '(:ignore t :which-key "help")
-		 "ht" 'tern-get-type
-		 "hd" 'tern-get-docs
-
-		 "r" 'tern-rename-variable)
-
-  :config
-  (add-hook 'js2-mode-hook 'tern-mode))
-
 (provide 'javascript)
 
 ;;; javascript.el ends here
