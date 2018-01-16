c650c5f483e172714c938d1261465ce5fefef2f7
Brian Hicks <brian@brianthicks.com>
2018-01-16 16:52:37 -0600

    fix whitespace in completion.el

--- setup/completion.el
+++ setup/completion.el
@@ -14,7 +14,7 @@
   (global-company-mode 1)
 
   (setq company-idle-delay 0.2
-	company-selection-wrap-around t)
+        company-selection-wrap-around t)
 
   (define-key company-active-map [tab] 'company-complete)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
