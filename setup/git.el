d295ab79a9a0bed8a8bbae7d8884634b4052f2a2
Brian Hicks <brian@brianthicks.com>
2018-02-09 22:08:57 -0600

    fill column 72 in git commit messages

--- setup/git.el
+++ setup/git.el
@@ -9,7 +9,10 @@
   (general-nmap :prefix "SPC"
                 "g" '(:ignore t :which-key "git")
                 "gs" 'magit-status
-                "gb" 'magit-blame))
+                "gb" 'magit-blame)
+
+  :config
+  (setq git-commit-fill-column 72))
 
 (use-package evil-magit
   :after magit)
