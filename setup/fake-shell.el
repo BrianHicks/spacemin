982a4a41ca8fa2235476cc7bbc852c666a180026
Brian Hicks <brian@brianthicks.com>
2018-03-15 14:35:04 -0500

    remove ANSI escapes in eshell (might be WIP?)

--- setup/fake-shell.el
+++ setup/fake-shell.el
@@ -15,6 +15,26 @@
 (general-nmap :prefix "<SPC>"
               "'" 'projectile-run-eshell)
 
+;; rails prints things like ^[[0G, which are not really valid ANSI escape
+;; sequences. Let's get rid of 'em.
+;;
+;; modified from https://emacs.stackexchange.com/questions/18457/stripping-stray-ansi-escape-sequences-from-eshell
+(defvar spacemin-ansi-escape-re
+  (rx (or ?\233 (and ?\e ?\[))
+    (char (?0 . ?9))
+    (char (?@ . ?_))))
+
+(defun spacemin-remove-ansi-escapes (beg end)
+  (save-excursion
+    (goto-char beg)
+    (while (re-search-forward spacemin-ansi-escape-re end t)
+      (replace-match ""))))
+
+(defun spacemin-eshell-remove-ansi-escapes ()
+  (spacemin-remove-ansi-escapes eshell-last-output-start eshell-last-output-end))
+
+(add-hook 'eshell-output-filter-functions 'spacemin-eshell-remove-ansi-escapes t)
+
 (provide 'fake-shell)
 
 ;;; fake-shell.el ends here
