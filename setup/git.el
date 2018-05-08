7a5a22c8c8e4a83d880cc4f2822ae16a87baf7c5
Brian Hicks <brian@brianthicks.com>
2018-05-07 21:48:07 -0500

    use magithub

--- setup/git.el
+++ setup/git.el
@@ -17,14 +17,14 @@
 (use-package evil-magit
   :after magit)
 
-(use-package magit-gh-pulls
+(use-package magithub
   :after magit
   :config
-  (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls))
+  (magithub-feature-autoinject t)
+  (setq magithub-clone-directory-default "~/code"))
 
 ;; TODO:
 ;;
-;; - github integration
 ;; - shortcut to jump to a Jenkins build for a branch
 
 (provide 'git)
