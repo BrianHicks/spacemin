2c4e26c74eee3cd3f815bd4cbf9f0b13ba6470e9
Brian Hicks <brian@brianthicks.com>
2018-06-13 15:18:19 -0500

    try fantasque sans
    good for something different

--- setup/display.el
+++ setup/display.el
@@ -68,7 +68,7 @@
 
 ;; fonts and ligatures
 (when (window-system)
-  (set-face-attribute 'default nil :height 130 :family "Fira Code")
+  (set-face-attribute 'default nil :height 160 :family "Fantasque Sans Mono")
   (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
 
 ;; transparent title bar
